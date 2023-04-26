4f33803ea00e2f1c31ba1aef5e5a145ba83a308f
andrewzw <yapzhewei2002@gmail.com>
2023-04-26 13:50:40 +1000

    Added public path for mercury upload

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // publicPath: '/cos30043/s103508895/Recipe-Finder_103508895',
 })
