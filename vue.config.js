b9dc157371e516b869beb9727d97dca8b51a0194
andrewzw <yapzhewei2002@gmail.com>
2023-04-26 15:19:20 +1000

    publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: '/cos30043/s103508895/Recipe-Finder_103508895',
+  // publicPath: '/cos30043/s103508895/6.3HD/Recipe-Finder_103508895',
 })
